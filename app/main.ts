d945b028ff947f61f8b8cc0701e1c680e218d335
idealley <samuelpouyt@gmail.com>
2016-11-06 00:15:49 +0100

    initial and final commit

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,8 @@
+import { setStatusBarColors } from "./utils/status-bar-util";
+
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
 
+setStatusBarColors();
 platformNativeScriptDynamic().bootstrapModule(AppModule);
